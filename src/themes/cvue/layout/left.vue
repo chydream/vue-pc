2bfb7de4ecd6149373cf6dd3a5d75a65c2164e09
chydream <710962261@qq.com>
2019-12-05 10:03:33 +0800

    细节修改

--- src/themes/cvue/layout/left.vue
+++ src/themes/cvue/layout/left.vue
@@ -64,7 +64,7 @@ export default {
   },
   mounted () {
     // this.getMenuData()
-    console.log(this.menu)
+    // console.log(this.menu)
   },
   methods: {
     goUrl (path) {
--- src/themes/cvue/views/videoNew/slide.vue
+++ src/themes/cvue/views/videoNew/slide.vue
@@ -1,15 +1,12 @@
 <template>
   <div>
-    <video-new></video-new> 111
+    
   </div>
 </template>
 <script>
 import videoNew from './video'
 export default {
-  name: 'video',
-  components: {
-    videoNew
-  }
+  name: 'slide'
 }
 </script>
 <style>
