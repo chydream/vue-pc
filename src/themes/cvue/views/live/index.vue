628ddfbbdeccb961f10d68e1f3d495dfc2ad8e15
chydream <5249291yong>
2020-11-06 14:44:52 +0800

    细节修改

--- src/themes/cvue/views/live/index.vue
+++ src/themes/cvue/views/live/index.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>dfsfsfs</div>
+  <div>
+    <video autoplay playsinline ref="videoRtc"></video>
+  </div>
 </template>
 <script>
+/* eslint-disable*/
 export default {
-  name: ''
+  name: '',
+  mounted () {
+    var aliWebrtc = new AliRtcEngine()
+    aliWebrtc.startPreview(this.$refs.videoRtc).then(() => {
+      console.log('success')
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
 }
 </script>
 <style scoped>
-
+@import url('../../../../../static/ali/index.css');
 </style>
